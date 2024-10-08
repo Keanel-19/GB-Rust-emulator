4874d05883c17a59e449ecde5f1db2200a626ed0
Mael Saillot <mael.saillot@telecomnancy.eu>
2024-10-08 23:58:15 +0200

    instruction block 3: interrupt

--- src/gb/cpu/instructions/block_3/interrupt.rs
+++ src/gb/cpu/instructions/block_3/interrupt.rs
@@ -1,19 +1,35 @@
-use crate::gb::cpu::{enums::Instruction, structs::RW, CpuContext};
+use crate::gb::cpu::{enums::Instruction, CpuContext};
 
 opcode!{
     reti (cpu: &mut CpuContext) {
-        todo!()
+        cpu.regs.z = cpu.hw.read(cpu.regs.sp);
+        cpu.regs.sp += 1;
+        Instruction::Void(load_w)
+    }
+    load_w (cpu: &mut CpuContext) {
+        cpu.regs.w = cpu.hw.read(cpu.regs.sp);
+        cpu.regs.sp += 1;
+        Instruction::Void(write_pc)
+    }
+    write_pc (cpu: &mut CpuContext) {
+        cpu.regs.pc = cpu.regs.wz();
+        cpu.regs.interrupt_enable = true;
+        Instruction::default()
     }
 }
 
 opcode!{
     di (cpu: &mut CpuContext) {
-        todo!()
+        cpu.regs.interrupt_enable = false;
+        cpu.fetch_pc()
     }
 }
 
 opcode!{
     ei (cpu: &mut CpuContext) {
-        todo!()
+        let next = cpu.fetch_cycle(cpu.regs.pc);
+        cpu.regs.pc += 1;
+        cpu.regs.interrupt_enable = true;
+        next
     }
 }
